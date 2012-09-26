021a174b0047f10cb4108a9ac4651f62e6c98294
Cory Wright <corywright@gmail.com>
2012-09-26 10:51:54 -0700

    add autocompletion to supernova

--- setup.py
+++ setup.py
@@ -31,5 +31,6 @@ setup(
         'console_scripts': [
             'supernova = supernova.executable:run_supernova',
             'supernova-keyring = supernova.executable:run_supernova_keyring'],
-        }
+        },
+    data_files=[('/etc/bash_completion.d/',['contrib/supernova-completion.bash'])]
     )
