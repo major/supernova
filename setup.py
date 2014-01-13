093f924ecf118cab0c5a41b0a515b6ac0be60b02
Major Hayden <major@mhtx.net>
2014-01-13 10:05:45 -0600

    Version 0.9.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='supernova',
-    version='0.8.0',
+    version='0.9.0',
     author='Major Hayden',
     author_email='major@mhtx.net',
     description="novaclient wrapper for multiple nova environments",
