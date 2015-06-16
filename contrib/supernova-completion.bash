59d5a36024c7334a1c18bed0ca9fc583d2c6beb1
Josh Guice <jguice@gmail.com>
2015-06-16 12:44:40 -0700

    fix(completion): don't match ini file comments

--- contrib/supernova-completion.bash
+++ contrib/supernova-completion.bash
@@ -4,7 +4,7 @@ _supernova()
 {
     local cur=${COMP_WORDS[COMP_CWORD]}
     local configs=$(cat "${XDG_CONFIG_HOME}"/supernova ~/.supernova ./.supernova 2> /dev/null)
-    local possibilities=$(echo "${configs}" | sed -n '/^\[.*\]/ s_\[\(.*\)\]_\1_p' | sort -u)
+    local possibilities=$(echo "${configs}" | sed -n '/^\[.*\]/ s_\[\(.*\)\].*_\1_p' | sort -u)
     COMPREPLY=( $(compgen -W "${possibilities}" -- $cur) )
 }
 complete -F _supernova supernova
